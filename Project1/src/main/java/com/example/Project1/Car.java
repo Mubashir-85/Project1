a570ae494e2307751b09a870383a509e66700a1c
Shaikh Mubashir <shaikhmubashirahmed123@gmail.com>
2026-05-23 18:45:09 +0530

    modify car class

--- Project1/src/main/java/com/example/Project1/Car.java
+++ Project1/src/main/java/com/example/Project1/Car.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class Car {
     private Engine engine;
-    @Autowired
+
     public Car(Engine engine){
         this.engine = engine;
     }
